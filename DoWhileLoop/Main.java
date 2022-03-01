36bf30ea683a401514ef298fdd893d96fea36160
Ttack412 <31516693+Ttack412@users.noreply.github.com>
2022-02-28 22:37:10 -0500

    Add files via upload

---
+++ DoWhileLoop/Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+
+    // 1. Create a do-while loop that prints out the numbers from 20 - 1. You can intitalize an int variable before the do-while loop
+  int myInt = 20;
+  do
+    {
+      System.out.println(myInt);
+      myInt--;
+    }
+  while(myInt > 0);
+    
+    }
+}
